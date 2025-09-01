3612614e0ce4b9849e21c9453f3dd0c7cb482988
Tornikee77 <tornikeesitashvili999@gmail.com>
2025-09-01 21:50:58 +0400

    feature/addingIcon

--- features/form/components/composites/CompanyBasics.tsx
+++ features/form/components/composites/CompanyBasics.tsx
@@ -26,6 +26,12 @@ const CompanyBasics = () => {
     <div className="max-w-[1222px] w-full bg-transparent mx-auto  rounded-[8px] px-[30px] py-[30px] flex flex-col border ">
       <div className="flex flex-col">
         <div className="flex items-center gap-2">
+          <Image
+            src="/images/svg/CompanyBasics.svg"
+            width={22}
+            height={22}
+            alt="logo"
+          />
           <h2 className="text-[27px] font-[600] text-[#000000]">
             კამპანიის საფუძვლები
           </h2>
